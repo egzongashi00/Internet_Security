0d227cc164b0c6017aef60a224dd7c0ad0237106
betimtasholli <betim.tasholli1@student.uni-pr.edu>
2021-02-12 21:39:55 +0100

    Main Gui

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-import tkinter as tk
-parent = tk.Tk()
-parent.geometry("400x250")
-email = tk.Label(parent, text = "Username").place(x = 90, y = 90)
-password =  tk.Label(parent, text = "Password").place(x = 90, y = 130)
-sbmitbtn = tk.Button(parent, text = "Log In", activebackground = "green", activeforeground = "blue").place(x = 190, y = 170)
-entry1 = tk.Entry(parent).place(x = 160, y = 90)
-entry2 = tk.Entry(parent, show="*").place(x = 160, y = 130)
-parent.mainloop()
+import tkinter 
+
+OPTIONS = [
+"Chrome",
+"Mozilla",
+"Internet Explorer"
+]
+
+window = tkinter.Tk()
+window.geometry('500x300')
+
+variable = tkinter.StringVar(window)
+variable.set(OPTIONS[0])
+text = tkinter.Label(window, text = "Zgjedhni njerin nga browseret").place(relx=0.5, rely=0.2,anchor='center')
+select = tkinter.OptionMenu(window, variable, *OPTIONS).place(relx=0.4, rely=0.4,anchor='center')
+kerkoButton = tkinter.Button(window, text="Kerko").place(relx=0.6, rely=0.4,anchor='center')
+
+def get():
+    print ("value is:" + variable.get())
+
+
+window.mainloop()
